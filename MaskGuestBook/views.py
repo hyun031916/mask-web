55dd1c65eaf896a28dca2325b409c8e2de66061f
ksh <hyun031916@gmail.com>
2021-05-31 00:22:23 +0900

    views.py에 카카오 채널 챗봇 기능 코드 작성

--- MaskGuestBook/views.py
+++ MaskGuestBook/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
 from .forms import GuestBookForm
 from .models import GuestBook
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def postGuestBook(request):
@@ -37,9 +39,31 @@ def index(request):
 def keyboard(request):
     return JsonResponse(
         {
-            'type': 'buttons',
-            'buttons': ['Mask란', '개발자 소개']
+            'type': 'text'
         }
     )
+
+@csrf_exempt
+def message(request):
+    answer = ((request.body).decode('utf8'))
+    return_json_str = json.loads(answer)
+    return_str = return_json_str['userRequest']['utterance']
+
+    if return_str == '테스트':
+        return JsonResponse({
+            'version':"2.0",
+            'template':{
+                'outputs':[{
+                    'simpleText': {
+                        'text': '테스트 성공'
+                    }
+                }],
+                'quickReplies':[{
+                    'label': 'first',
+                    'action': 'message',
+                    'messageText': '처음으로'
+                }]
+            }
+        })
 
 
